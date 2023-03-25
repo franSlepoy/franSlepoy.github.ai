53ca7abeb4390109ee53014afe994ba3c22ae183
franSlepoy <118859799+franSlepoy@users.noreply.github.com>
2023-03-25 12:57:39 -0300

    initial commit

---
+++ javaScript.js
@@ -0,0 +1,18 @@
+document.getElementById("codigo").onmouseover = () =>{
+    document.getElementById("bienvenida").innerHTML = "En este momento me encuentro en la última etapa de los cursos: Full Stack Node Js de Digital House y Frontend con Javascript de Argentina programa. Tengo experiencia con las siguientes tecnologias: HTML, CSS, JavaScript, Node JS, Express, MVC, EJS, Git Hub y SQL. Estoy realizando proyectos web de manera freelance. Puedes ver estos proyectos y algunas prácticas donde aplico los conocimientos mencionados en mi repositorio de GitHub (link en pie de página) "
+};
+document.getElementById("grupo").onmouseover = () =>{
+    document.getElementById("bienvenida").innerHTML = " En mi experiencia con el proyecto integrador grupal de Digital House, creo tener algunos conocimientos más en la parte del front, pero también me interesa mucho el back. Si pudiera eleguir, me gustaría intercalar tareas y poder aprender ambas funciones. En cuanto a mis habilidades blandas, tengo 15 años de experiencia liderando e integrando grupos musicales pequeños de entre 3 y 8 integrantes. He partcipado en trabajos con orquestas de hasta 150 integrantes. Tengo amplia experiencia en la autogestión de proyectos. Además de mis propios emprendimientos he liderado una productora con la que realizamos ciclos y producción de contenido. En todos estos trabajos he redactado y gestionado premios, subsidios y sponsors."
+};
+document.getElementById("lugar").onmouseover = () =>{
+    document.getElementById("bienvenida").innerHTML = "Resido actualmente en la ciudad de Buenos Aires desde hace 19 años. Nací en La Paz (Bolivia) en 1985, donde mi padre se encontraba trabajando. Mi nacionalidad es Argentina y me crié en la ciudad de Resistencia, Chaco. "
+};
+document.getElementById("musica").onmouseover = () =>{
+    document.getElementById("bienvenida").innerHTML = "Como musico profecional he tocado en algunos de los más importantes escenarios de Buenos Aires como el Teatro Colón, La Usina del Arte, Museo de Arte Moderno de Buenos Aires, Museo Nacional de Arte Decorativo de Buenos Aires, entre otros. He realizado giras llevando mi música por Europa, Uruguay y Chile. Tengo editados 7 discos y uno de ellos me llevó a ganar un premio del FNA (Fondo Nacional de las Artes) en la categoría ¨jazz y música contemporánea¨, fui elegido en dos oportunidades como guitarrista del año por la encuesta anual a periodistas especializados de la revista El Intruso. Links: youtube.com/franciscoslepoy - instagram.com/franciscoslepoy = franciscoslepoy.bandcamp.com - https://open.spotify.com/artist/3Y26VMJHcHzDOIaTxDYKp0 "
+};
+document.getElementById("email").onmouseover = () =>{
+    document.getElementById("bienvenida").innerHTML = " e-mail: slepoyfrancisco@gmail.com o click en Contacto (pie de página)"
+}  
+document.getElementById("titulo").onclick = () =>{
+    document.body.style.backgroundColor = "rgb(203, 203, 203)"
+};
